bc16ab18d2aa9218b14069a555326b1d6b0f7896
JoaoPedropb2005 <jppb2005normal@gmail.com>
2026-02-13 10:46:41 -0300

    Spring config for project

---
+++ src/main/java/com/nextage/nextagebb/NextagebbApplication.java
@@ -0,0 +1,13 @@
+package com.nextage.nextagebb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextagebbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextagebbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextage/nextagebb/NextagebbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextage.nextagebb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextagebbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
